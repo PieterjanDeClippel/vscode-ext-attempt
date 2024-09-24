0d199ee46324139b6eeca904913fe31912bc8661
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-09-24 21:49:25 +0200

    Another fruitless attempt

--- apps/exte/src/main.ts
+++ apps/exte/src/main.ts
@@ -1,1 +1,13 @@
-console.log('Hello World');
+import { commands, ExtensionContext, window } from 'vscode';
+
+// On activation
+export function activate(context: ExtensionContext) {
+  // Register command "start" 
+  commands.registerCommand('sayhello', () => {
+    window.showInformationMessage('Hello World');
+  })
+}
+
+// F5
+// ctrl+P
+// >sayHello
