c1324297d1125dd8be9921d1e5284f1f0125b5ed
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-09-24 21:32:21 +0200

    Initial commit

---
+++ apps/exte-e2e/jest.config.ts
@@ -0,0 +1,16 @@
+export default {
+  displayName: 'exte-e2e',
+  preset: '../../jest.preset.js',
+  setupFiles: ['<rootDir>/src/test-setup.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/exte-e2e',
+};
